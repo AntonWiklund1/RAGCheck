ea6a5f61a41f41580c8299c6bbb1931b34817958
Anton <awiklund76@gmail.com>
2024-11-08 23:38:12 +0200

    main.py with example usage

---
+++ main.py
@@ -0,0 +1,36 @@
+from src.rag import RAGSystem
+from src.evaluation import evaluate_rag_system
+from src.llms import gpt_4o_mini, ministral_3b, gemini_1_5_flash, llama_3_2_1B
+
+def main():
+    # Initialize the RAG system
+    rag = RAGSystem(
+        model_name="gpt-4o-mini",
+        data_dir='data/documents',
+        persist_dir='data/index_storage'
+    )
+    
+    # Build the index
+    rag.build_index()
+    
+    # # Save the index for future use
+    rag.save_index()
+    
+    # Load the index
+    rag.load_index()
+    
+    # Optional: Test a query
+    # response = rag.query("What was the main focus of the CYCLADES project?")
+    # print(response)
+
+    # Test the RAG system
+    average_score = evaluate_rag_system(
+        rag_query_fn=rag.query,
+        test_set_path="data/test.csv",
+        batch_size=20,
+        num_tests=100,
+        evaluator_model=gemini_1_5_flash
+    )
+
+if __name__ == "__main__":
+    main()
